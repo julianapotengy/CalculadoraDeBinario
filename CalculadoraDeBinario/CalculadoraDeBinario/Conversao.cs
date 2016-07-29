3072885382061133112b316f109ae4f52149b135
Juliana Potengy <juliana.potengy@cejoseleitelopes.com.br>
2016-07-29 11:08:37 -0300

    projeto
    Decimal pra binário e hexadecimal
    Binário pra decimal e hexadecimal
    Hexadecimal pra decimal e binário

---
+++ CalculadoraDeBinario/CalculadoraDeBinario/Conversao.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeBinario
+{
+    class Conversao
+    {
+        public static string InverterString(string str)
+        {
+            int tamanho = str.Length;
+            char[] caracteres = new char[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                caracteres[i] = str[tamanho - 1 - i];
+            }
+            return new string(caracteres);
+        }
+
+        public static int BinarioParaDecimal(string valorBinario)
+        {
+            int expoente = 0;
+            int numero;
+            int soma = 0;
+
+            string numeroInvertido = InverterString(valorBinario);
+
+            for (int i = 0; i < numeroInvertido.Length; i++)
+            {
+                numero = Convert.ToInt32(numeroInvertido.Substring(i, 1));
+                soma += numero * (int)Math.Pow(2, expoente);
+                expoente++;
+            }
+            return soma;
+        }
+
+        public static string BinarioParaHex(string valorBinario)
+        {
+            string valorHex= "";
+           valorHex = Convert.ToInt32(valorBinario, 2).ToString("X");
+
+            return valorHex;
+        }
+
+        public static string DecimalParaBinario(string valorDecimal)
+        {
+            string valorBinario = "";
+            int dividendo = Convert.ToInt32(valorDecimal);
+
+            if (dividendo == 0 || dividendo == 1)
+            {
+                return Convert.ToString(dividendo);
+            }
+            else
+            {
+                while (dividendo > 0)
+                {
+                    valorBinario += Convert.ToString(dividendo % 2);
+                    dividendo = dividendo / 2;
+                }
+                return InverterString(valorBinario);
+            }
+        }
+
+        public static string DecimalParaHex(int valorDecimal)
+        {
+            string valorHex = "";
+            valorHex = valorDecimal.ToString("X");
+
+            return valorHex;
+        }
+
+        public static string HexParaBinario(string valorHex)
+        {
+            string valorBinario = "";
+            valorBinario = Convert.ToString(Convert.ToInt32(valorHex, 16), 2);
+
+            return valorBinario;
+        }
+
+        public static string HexParaDecimal(string valorHex)
+        {
+            string valorDecimal = "";
+            valorDecimal = Convert.ToString(Convert.ToInt32(valorHex, 16));
+
+            return valorDecimal;
+        }
+    }
+}
---
+++ CalculadoraDeBinario/CalculadoraDeBinario/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraDeBinario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string valorBinario = Conversao.DecimalParaBinario(textBox1.Text);
+                textBox2.Text = valorBinario;
+
+                string valorHex = Conversao.DecimalParaHex(int.Parse(textBox1.Text));
+                textBox3.Text = valorHex;
+            }
+            catch(Exception)
+            {
+                textBox2.Text = "Tente novamente";
+                textBox3.Text = "Tente novamente";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int valorDecimal = Conversao.BinarioParaDecimal(textBox2.Text);
+                textBox1.Text = Convert.ToString(valorDecimal);
+
+                string valorHex = Conversao.BinarioParaHex(textBox2.Text);
+                textBox3.Text = valorHex;
+            }
+            catch(Exception)
+            {
+                textBox1.Text = "Tente novamente";
+                textBox3.Text = "Tente novamente";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string valorBinario = Conversao.HexParaBinario(textBox3.Text);
+            textBox2.Text = valorBinario;
+
+            string valorDecimal = Conversao.HexParaDecimal(textBox3.Text);
+            textBox1.Text = valorDecimal;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
